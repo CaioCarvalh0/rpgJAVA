abdf58db687080ca0b40a16c5ef5ca26dc46b938
Caio de Carvalho Oliveira <caio.oliveira@vitalsis.com.br>
2024-06-10 16:53:44 -0300

    feat:Adiciona Telas

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
+import view.MainStart;
+
 public class Main {
     public static void main(String[] args) {
+        MainStart mainStart = new MainStart();
+        mainStart.setVisible(true);
     }
 }
